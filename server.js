5877becf60301b4038b912d9952a0982b9dad7eb
PexArt-web <pexart74@gmail.com>
2025-04-23 02:38:28 +0100

    startup_planet

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const PORT = 8000;
 const app = express();
-const PORT = 8000
+const { log } = console;
+const { startups } = require("./data/data.js");
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
 
-app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}`))
+app.get("/api", (req, res) => {
+  res.status(200).json(startups);
+});
